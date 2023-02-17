d40c16b21dfb331c267d5478abe8d3c1da150acd
ammar alibrahim <ammaryasir8088mail.com>
2023-02-17 19:05:02 +0300

    show add to cart data in my basket dropdown

--- public/script.js
+++ public/script.js
@@ -1,51 +1,42 @@
-let menu = document.querySelector('#menu-bar');
-let navbar = document.querySelector('.navbar');
+let menu = document.querySelector("#menu-bar");
+let navbar = document.querySelector(".navbar");
 
-menu.onclick = () =>{
+menu.onclick = () => {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+};
 
-  menu.classList.toggle('fa-times');
-  navbar.classList.toggle('active');
+window.onscroll = () => {
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
 
-}
-
-
-
-
-window.onscroll = () =>{
-
-  menu.classList.remove('fa-times');
-  navbar.classList.remove('active');
-
-  if(window.scrollY > 60){
-    document.querySelector('#scroll-top').classList.add('active');
-  }else{
-    document.querySelector('#scroll-top').classList.remove('active');
+  if (window.scrollY > 60) {
+    document.querySelector("#scroll-top").classList.add("active");
+  } else {
+    document.querySelector("#scroll-top").classList.remove("active");
   }
-
-}
-
+};
 
 // Get all the "Add to Cart" buttons
-const addButtons = document.querySelectorAll('.creat');
+const addButtons = document.querySelectorAll(".creat");
 
 // Get the cart items container
-const cartItems = document.getElementById('cart-items');
+const cartItems = document.getElementById("cart-items");
 
 // Add an event listener to each button
-addButtons.forEach(button => {
-  button.addEventListener('click', () => {
+addButtons.forEach((button) => {
+  button.addEventListener("click", () => {
     // Get the item details
     const item = button.parentNode;
-    const itemName = item.querySelector('h3').innerText;
-    const itemPrice = item.querySelector('.price').innerText;
-    const itemImage = item.querySelector('img').src;
+    const itemName = item.querySelector("h3").innerText;
+    const itemPrice = item.querySelector(".price").innerText;
+    const itemImage = item.querySelector("img").src;
 
-    
     // Create a new item element
-   // Create a new item element
-const newItem = document.createElement('div');
-newItem.classList.add('cart-item');
-newItem.innerHTML = `
+    // Create a new item element
+    const newItem = document.createElement("div");
+    newItem.classList.add("cart-item");
+    newItem.innerHTML = `
   <img src="${itemImage}">
   <div class="cart-item-details">
     <span class="cart-item-name">${itemName}</span>
@@ -54,30 +45,28 @@ newItem.innerHTML = `
   <button class="remove-button">Remove</button>
 `;
 
-    
     // Add the new item to the cart items container
     cartItems.appendChild(newItem);
-    
+
     // Show the dropdown content
-    document.querySelector('.dropdown-content').style.display = 'block';
+    document.querySelector(".dropdown-content").style.display = "block";
   });
 });
 
 // Add an event listener to the "Remove" buttons
-cartItems.addEventListener('click', event => {
-  if (event.target.classList.contains('remove-button')) {
+cartItems.addEventListener("click", (event) => {
+  if (event.target.classList.contains("remove-button")) {
     event.target.parentNode.remove();
   }
-  
+
   // Hide the dropdown content if there are no items left
   if (cartItems.children.length === 0) {
-    document.querySelector('.dropdown-content').style.display = 'none';
+    document.querySelector(".dropdown-content").style.display = "none";
   }
 });
 
-
-
-
-
-
-
+window.onbeforeunload = function() {
+  if (cartItems.children.length > 0) {
+    return 'Are you sure you want to leave? Your cart will be emptied.';
+  }
+};
